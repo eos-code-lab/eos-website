d928785555fb7b64f662046710bd307796cd7a56
Andrei-Mihai Popa <andreim_popa@yahoo.com>
2018-01-12 14:04:13 +0200

    recaptcha
    added recaptcha (php)

--- store.php
+++ store.php
@@ -5,28 +5,57 @@ $conn    = Connect();
 $name    = $conn->real_escape_string($_POST['name']);
 $email   = $conn->real_escape_string($_POST['email']);
 $message = $conn->real_escape_string($_POST['message']);
-$query   = "INSERT into eos_projects (name,email,message) VALUES('" . $name . "','" . $email . "','" . $message . "')";
-$success = $conn->query($query);
+$email_to="andreim_popa@yahoo.com";
 
-if (!$success) {
-    die("Couldn't enter data: ".$conn->error);
+if(isset($_POST['submit'])){
+    
+    if(isset($_POST['g-recaptcha-response']) && !empty($_POST['g-recaptcha-response'])){
 
-}
-else
-{
-    $to = "andreim_popa@yahoo.com";
-    $from = $_POST['email']; 
-    $name = $_POST['name'];
-    $subject = "EOS";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'] ."\n\n". "My email address is " . $_POST['email'];
+    //get recaptcha verify response data
+    $secretkey='6LeJJkAUAAAAAPJUIcJWeBSVDWqqhbynDAeV1ii-';
+    $verifyResponse = file_get_contents('https://www.google.com/recaptcha/api/siteverify?secret='.$secretkey.'&response='.$_POST['g-recaptcha-response']);
+    $responseData = json_decode($verifyResponse);
 
-    $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    echo "<b>Mail Sent. Thank you " . $name . ", we will contact you shortly.</b>";
+    //recaptcha success
+    if($responseData->success) {
+    
+      $pattern = '/[\r\n]|Content-Type:|Bcc:|Cc:/i';
+      if (preg_match($pattern, $name) || preg_match($pattern, $email))
+        die("Header injection detected");
+        
+      //Sanitize subject
+      $name = str_ireplace(array("\r", "\n", "%0a", "%0d"), '', stripslashes($name));
+        $query   = "INSERT into eos_projects (name,email,message) VALUES('" . $name . "','" . $email . "','" . $message . "')";
+        $success = $conn->query($query);
+      // Validate e-mail
+      $emailIsValid = filter_var($email, FILTER_VALIDATE_EMAIL);
+        
+        if($emailIsValid){
+        
 
+        $content = 'From'.': '.$email."\r\n";
+        $content .= $name. ' wrote you the following message'.": \r\n\r\n";
+        $content .= $message;
 
-echo "<b>Thank You For Contacting Us </b><br>";
+        $headers = "Content-type:text/plain;charset=UTF-8";
+        $headers .= "From: ".$email;
+        $headers .= "Return-Path: $email_to";
+        $headers .= "Reply-To: $email";
 
-$conn->close();
+        //send email
+        $subject="EOS";
+        mail($email_to,$subject,$content,$headers);
+        echo 'mail sent successfully';
+            
+      }
+      else
+        //Error actions
+        echo "Could not send mail.";
+    }
+       else
+           echo "Please fill out the captcha.";
+    }
+    else
+          echo "Please fill out the captcha.";
 }
 ?>
