107302eb6a2c7a1500f837188fd115f3ecf7eb42
Claudia Ifrim <ifrim.claudia@gmail.com>
2025-07-22 23:51:47 +0300

    update footer to use tailwind

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,21 +1,54 @@
 // Site footer
 
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { faArrowCircleUp } from '@fortawesome/free-solid-svg-icons';
+
 const Footer = () => {
     const curretYear = new Date().getFullYear();
 
     return (
-        <footer className="footer text-white">
-            <div className="container-fluid">
-                <div className="d-flex justify-content-start">
-                    <p className="p-2">
-                        <small><strong>&copy; {curretYear} EOS CODE LAB - All Rights Reserved.</strong></small>
+        <footer className="bg-black bg-opacity-90 text-white py-4">
+            <div className="md:container md:mx-auto px-4">
+                <div className="flex flex-col md:flex-row justify-between items-center">
+                    <p className="mb-4 md:mb-0">
+                        <small className="font-bold">&copy; {curretYear} EOS CODE LAB - All Rights Reserved.</small>
                     </p>
-                    <div className="ml-auto p-2">
-                        <ul className="list-inline">
-                            <li className="list-inline-item"><a target="_blank" href="https://www.facebook.com/EOSCodeLab/"><i className="fab fa-facebook-f"></i></a></li>
-                            <li className="list-inline-item"><a target="_blank" href="https://twitter.com/CodeEos/"><i className="fab fa-twitter"></i></a></li>
-                            <li className="list-inline-item"><a target="_blank" href="https://www.linkedin.com/company/eos-code-lab/"><i className="fab fa-linkedin-in"></i></a></li>
-                            <li className="list-inline-item"><a href="#"><i className="fas fa-arrow-circle-up"></i></a></li>
+                    <div>
+                        <ul className="flex space-x-4">
+                            <li>
+                                <a 
+                                    target="_blank" 
+                                    href="https://www.facebook.com/EOSCodeLab/"
+                                >
+                                    <FontAwesomeIcon icon={faFacebookF} />
+                                </a>
+                            </li>
+                            <li>
+                                <a 
+                                    target="_blank" 
+                                    href="https://twitter.com/CodeEos/"
+                                >
+                                    <FontAwesomeIcon icon={faTwitter} />
+                                </a>
+                            </li>
+                            <li>
+                                <a 
+                                    target="_blank" 
+                                    href="https://www.linkedin.com/company/eos-code-lab/"
+                                >
+                                    <FontAwesomeIcon icon={faLinkedinIn} />
+                                </a>
+                            </li>
+                            <li>
+                            <Link 
+                                    href="#" 
+                                    className="text-white hover:text-gray-300"
+                                >
+                                    <FontAwesomeIcon icon={faArrowCircleUp} />
+                                </Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
